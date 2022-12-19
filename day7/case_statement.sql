eddecc69753c86751fa77d5bddc7a036a422624c
paras <parasguglani1@gmail.com>
2022-12-19 11:26:05 +0530

    day 7

---
+++ day7/case_statement.sql
@@ -0,0 +1,67 @@
+create table employee
+(
+emp_id    number ,
+Fname  varchar2(10) NOT NULL,
+Eaddress  varchar2(10),
+salary    number,
+hiredate DATE,
+CONSTRAINT PK PRIMARY KEY(emp_id)
+);
+
+desc employee;
+
+insert into employee values(1,'Paras','Meerut',40000,DATE'2001-08-22');
+
+insert into employee values(2,'Paras2','delhi1',80000,DATE'2020-08-22');
+insert into employee values(3,'Paras3','delhi2',90000,DATE'2021-08-22');
+insert into employee values(4,'Paras4','delhi3',10003,DATE'2022-08-22');
+insert into employee values(5,'Paras5','delhi4',100001,DATE'2023-08-22');
+insert into employee values(6,'Paras6','delhi5',100002,DATE'2024-08-22');
+
+select * from employee;
+
+
+Case salary
+ when salary > 40000 then
+    update employee set Eaddress = 'delhi';
+else
+    update employee set Eaddress = 'Meerut';
+end case;
+
+
+
+CASE Eaddress
+ when 'Meerut' then
+    update employee set Eaddress = 'delhi' when Eaddress ='Meerut'
+else
+    update employee set Eaddress = 'Meerut'  when Eaddress ='Meerut'
+    END case;
+
+
+
+
+Case when salary > 40000 then
+    update employee set Eaddress = 'delhi';
+else
+    update employee set Eaddress = 'Meerut';
+end case;
+
+
+
+select emp_id, Fname, Eaddress, salary, hiredate,
+case
+when salary between 40000 and 90000 then 'low'
+when salary between 90000 and 100000 then 'medium'
+else 'high'
+end as salary_range
+from employee;
+
+--working
+select emp_id, Fname, Eaddress, salary, hiredate,
+case
+when salary between 40000 and 90000 then 'low'
+when salary between 90000 and 100000 then 'medium'
+else 'high'
+end as salary_range
+from employee;
+
